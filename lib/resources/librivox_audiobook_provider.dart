511327ea18364cf939ac273eda74a27ad556ecb3
Brian Cherin <brian.cherin@gmail.com>
2021-11-11 21:52:44 -0500

    Retrieve list of chapters for given audiobook

--- lib/resources/librivox_audiobook_provider.dart
+++ lib/resources/librivox_audiobook_provider.dart
@@ -11,7 +11,7 @@ import 'package:http/http.dart' as http;
 class LibrivoxAudiobookProvider extends AudiobookProvider {
 
   String baseArchiveUrl = "https://archive.org/advancedsearch.php?q=collection:(librivoxaudio)&fl[]=avg_rating,description,downloads,identifier,item_size,title,creator&sort[]=addeddate+desc&output=json";
-
+  String baseChaptersInfoUrl = "https://archive.org/metadata";
 
 
   @override
@@ -36,14 +36,38 @@ class LibrivoxAudiobookProvider extends AudiobookProvider {
   }
 
   @override
-  Future<List<Chapter>> fetchChapters({Audiobook audiobook}) {
-    // TODO: Get actual data
-    List<Chapter> list = [
+  Future<List<Chapter>> fetchChapters({Audiobook audiobook}) async {
+    String librivoxItemId = (audiobook as LibrivoxAudiobook).librivoxItemId;
+    String chaptersInfoUrl = baseChaptersInfoUrl + "/$librivoxItemId/files";
+
+    List<Chapter> chapters = [];
+
+    final response = await http.get(Uri.parse(chaptersInfoUrl));
+    if (response.statusCode == 200) {
+      // Request succeeded.
+      final audiobookFilesListJson = jsonDecode(response.body)["result"];
+
+      // This contains a list of all the files associated with the audiobook.
+      // We want only the files that describe a chapter/track for the book.
+      for (final fileJson in audiobookFilesListJson) {
+        if (fileJson["source"] == "original" && fileJson["track"] != null) {
+          chapters.add(LibrivoxChapter.fromJson(fileJson, librivoxItemId));
+        }
+      }
+
+
+    } else {
+      //TODO: Do something on request failure
+    }
+
+
+
+ /*   List<Chapter> list = [
       LibrivoxChapter(title: "Chapter 1", trackUrl: "https://archive.org/download/dracula_librivox/dracula_01_stoker.mp3"),
       LibrivoxChapter(title: "Chapter 2", trackUrl: "https://archive.org/download/dracula_librivox/dracula_02_stoker.mp3"),
       LibrivoxChapter(title: "Chapter 3", trackUrl: "https://archive.org/download/dracula_librivox/dracula_03_stoker.mp3"),
-    ];
-    return Future.value(list);
+    ];*/
+    return Future.value(chapters);
   }
 
   String getArchiveQueryUrl({int offset, int limit}) {
--- lib/resources/models/chapter.dart
+++ lib/resources/models/chapter.dart
@@ -1,26 +1,63 @@
-
-
 import 'package:flutter/cupertino.dart';
 
 class Chapter {
   final String title;
   final String fileName;
+  final int chapterNumber;
+  final double durationSeconds;
 
   Chapter({
-    @required this.title,
+    this.title,
     this.fileName,
+    this.chapterNumber,
+    this.durationSeconds,
   });
-
 }
 
+//  "https://archive.org/download/<book_id>/<track_file_name>"
+String baseLibrivoxChapterUrl = "https://archive.org/download";
 class LibrivoxChapter extends Chapter {
   final String title;
   final String fileName;
+  final int chapterNumber;
+  final double durationSeconds;
+
   final String trackUrl;
+  final String librivoxItemId; // The id of the audiobook
 
   LibrivoxChapter({
-    @required this.title,
+    this.title,
     this.fileName,
-    @required this.trackUrl,
-  }) : super(title: title, fileName: fileName);
+    this.chapterNumber,
+    this.durationSeconds,
+    this.trackUrl,
+    this.librivoxItemId,
+  }) : super(
+            title: title,
+            fileName: fileName,
+            chapterNumber: chapterNumber,
+            durationSeconds: durationSeconds);
+
+
+  factory LibrivoxChapter.fromJson(Map<String, dynamic> json, String librivoxItemId) {
+    var fileName = json["name"];
+    return LibrivoxChapter(
+      title: json["title"],
+      fileName: fileName,
+      chapterNumber: int.parse((json["track"] as String).split("/")[0]), // going from "1/27" to 1
+      durationSeconds: double.parse(json["length"]),
+      trackUrl: "$baseLibrivoxChapterUrl/$librivoxItemId/$fileName",
+      librivoxItemId: librivoxItemId,
+    );
+  }
+
+/** example JSON
+ * {"name":"dracula_01_stoker.mp3","source":"original","format":"128Kbps MP3",
+ * "creator":"Bram Stoker","album":"Dracula","title":"Chapter 01","track":"1/27",
+ * "md5":"b0377eb776152a4499db00a6ce62f023","mtime":"1147237781","size":"35490126",
+ * "crc32":"62e678ad","sha1":"3418d869e89ddf8a8ebd7711dc36ba653bcd5b38","length":"2218",
+ * "height":"0","width":"0","genre":"Speech","artist":"Bram Stoker","external-identifier-match-date":"acoustid:2019-04-04T21:23:19Z",
+ * "external-identifier":"urn:acoustid:f0e91e3f-3bdb-4b5a-b3ea-478185795904"}
+ */
+
 }
