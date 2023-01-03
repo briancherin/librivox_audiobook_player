a1fb8ce30816c0a68dc01ea4f47f6ebb3dda0410
Brian Cherin <brian.cherin@gmail.com>
2023-01-03 13:19:19 -0500

    #1 Prevent slider movement from resuming playback when paused

--- lib/screens/now_playing/bloc/now_playing_state.dart
+++ lib/screens/now_playing/bloc/now_playing_state.dart
@@ -9,10 +9,11 @@ class NowPlayingState extends Equatable {
   final bool audiobookIsPlaying;
   final bool chaptersLoaded;
   final int currentChapterIndex;
+  final bool userIsMovingAudioSlider;
 
-  NowPlayingState({@required this.currentState, this.audiobook, this.currentPositionMillis, this.audiobookIsPlaying, this.chaptersLoaded = false, this.currentChapterIndex});
+  NowPlayingState({@required this.currentState, this.audiobook, this.currentPositionMillis, this.audiobookIsPlaying, this.chaptersLoaded = false, this.currentChapterIndex, this.userIsMovingAudioSlider = false});
 
-  NowPlayingState copyWith({NowPlayingState currentState, Audiobook audiobook, double currentPositionMillis, bool audiobookIsPlaying, bool chaptersLoaded, int currentChapter}) {
+  NowPlayingState copyWith({NowPlayingState currentState, Audiobook audiobook, double currentPositionMillis, bool audiobookIsPlaying, bool chaptersLoaded, int currentChapter, bool userIsMovingAudioSlider}) {
     return NowPlayingState(
       currentState: currentState ?? this.currentState,
       audiobook: audiobook ?? this.audiobook,
@@ -20,11 +21,12 @@ class NowPlayingState extends Equatable {
       audiobookIsPlaying: audiobookIsPlaying ?? this.audiobookIsPlaying,
       chaptersLoaded: this.chaptersLoaded,
       currentChapterIndex: currentChapter ?? this.currentChapterIndex,
+      userIsMovingAudioSlider: userIsMovingAudioSlider ?? this.userIsMovingAudioSlider,
     );
   }
 
   @override
-  List<Object> get props => [currentState, audiobook, currentPositionMillis, audiobookIsPlaying, currentChapterIndex];
+  List<Object> get props => [currentState, audiobook, currentPositionMillis, audiobookIsPlaying, currentChapterIndex, userIsMovingAudioSlider];
 }
 
 class NowPlayingInitial extends NowPlayingState {}
